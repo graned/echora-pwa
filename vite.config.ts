82d3d64bb27b4502c4d4e36248a8d7a2d7d6f8e5
Eduardo Anaya <gran_ed@hotmail.com>
2025-04-30 11:22:07 +0200

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
-    }
+  optimizeDeps: {
+    include: ['@mui/icons-material'],
+    exclude: ['@mui/material']
   },
   server: {
-    port: 5173,
-    strictPort: true,
-    host: true,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
+    watch: {
+      usePolling: true
     }
   }
 })
